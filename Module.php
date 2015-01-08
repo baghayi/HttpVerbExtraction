05c399056e104b7075b411d76c897d2a49efd492
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-09 02:40:51 +0330

    Initial methods added to Module class

--- Module.php
+++ Module.php
@@ -1,8 +1,23 @@
 <?php
 namespace HttpVerbExtraction;
 
-class Module {
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
-
-
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
 }
+
+

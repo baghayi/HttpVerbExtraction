bbeadadc6b7f5a2370891f897fbcde2c51be3fc3
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-10 03:09:15 +0330

    Defined Data initializer and its related data provider class

--- Module.php
+++ Module.php
@@ -4,6 +4,8 @@ namespace HttpVerbExtraction;
 use HttpVerbExtraction\Initializer\QueryParams;
 use HttpVerbExtraction\InitializerValue\QueryParams as QueryParamsValue;
 use HttpVerbExtraction\DispatchableInterface;
+use HttpVerbExtraction\InitializerValue\Data as DataValue;
+use HttpVerbExtraction\Initializer\Data;
 
 class Module
 {
@@ -35,12 +37,28 @@ class Module
                     if($service instanceof QueryParams)
                         $service->setQueryParams($params->get());
                 },
+
+                Data::class => function($service, $sm){
+                    if(!$service instanceof DispatchableInterface)
+                        return;
+
+                    $data = $sm->get(DataValue::class);
+                    if($service instanceof Data)
+                        $service->setData($data->get());
+
+                },
             ], // initializers
 
             'factories' => [
                 QueryParamsValue::class => function($sm){
                     $event = $sm->get('Application')->getMvcEvent();
                     return new QueryParamsValue($event);
+                },
+
+                DataValue::class => function($sm){
+                    $pluginManager = $sm->get('ControllerPluginManager');
+                    $bodyParams = $pluginManager->get('bodyParams');
+                    return new DataValue($bodyParams);
                 },
             ]
         ];

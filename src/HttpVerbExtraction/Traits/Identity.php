7d81da7ed9cfb280a07e337cdeb74b134c879c89
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-11 04:48:39 +0330

    Add docblock

--- src/HttpVerbExtraction/Traits/Identity.php
+++ src/HttpVerbExtraction/Traits/Identity.php
@@ -6,6 +6,14 @@ use ZF\Rest\ResourceEvent;
 trait Identity {
 
     
+    /**
+     * Retrieve the identity, if any
+     *
+     * Proxies to the resource event to find the identity, if not already
+     * composed, and composes it.
+     *
+     * @return null|\ZF\MvcAuth\Identity\IdentityInterface
+     */
     public function getIdentity(ResourceEvent $event)
     {
         return $event->getIdentity();

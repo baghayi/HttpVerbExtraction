a349e8cfdab944e5b7160f16fedc4fe56c0da65a
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-12 06:19:29 +0330

    Classmap file added

---
+++ autoload_classmap.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    'HttpVerbExtraction\Rest\AbstractResourceListener'  => __DIR__ . '//src/HttpVerbExtraction/Rest/AbstractResourceListener.php',
+    'HttpVerbExtraction\Service\EntityClass'            => __DIR__ . '//src/HttpVerbExtraction/Service/EntityClass.php',
+    'HttpVerbExtraction\Service\CollectionClass'        => __DIR__ . '//src/HttpVerbExtraction/Service/CollectionClass.php',
+    'HttpVerbExtraction\Service\ControllerName'         => __DIR__ . '//src/HttpVerbExtraction/Service/ControllerName.php',
+    'HttpVerbExtraction\DispatchableInterface'          => __DIR__ . '//src/HttpVerbExtraction/DispatchableInterface.php',
+    'HttpVerbExtraction\Traits\Identity'                => __DIR__ . '//src/HttpVerbExtraction/Traits/Identity.php',
+    'HttpVerbExtraction\Traits\InputFilter'             => __DIR__ . '//src/HttpVerbExtraction/Traits/InputFilter.php',
+    'HttpVerbExtraction\Traits\Identifier'              => __DIR__ . '//src/HttpVerbExtraction/Traits/Identifier.php',
+    'HttpVerbExtraction\Traits\QueryParams'             => __DIR__ . '//src/HttpVerbExtraction/Traits/QueryParams.php',
+    'HttpVerbExtraction\Traits\Data'                    => __DIR__ . '//src/HttpVerbExtraction/Traits/Data.php',
+    'HttpVerbExtraction\Factory\EntityClassFactory'     => __DIR__ . '//src/HttpVerbExtraction/Factory/EntityClassFactory.php',
+    'HttpVerbExtraction\Factory\CollectionClassFactory' => __DIR__ . '//src/HttpVerbExtraction/Factory/CollectionClassFactory.php',
+    'HttpVerbExtraction\Module'                         => __DIR__ . '//Module.php',
+);

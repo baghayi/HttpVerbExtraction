d40a620e4de9f34b250f8a5773ebc69e047245af
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-14 02:25:27 +0330

    Add a condition to check a directory deeper for autoload file

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -4,6 +4,8 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')){
     require_once __DIR__ . '/../vendor/autoload.php';
 } elseif (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
     require_once __DIR__ . '/../../../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../../../vendor/autoload.php';
 } else{
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }

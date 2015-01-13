1a2635250a83e9e4389b51fd3a3c3c4d9cfbb59a
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-13 20:37:14 +0330

    Tested identifer trait

---
+++ tests/HttpVerbExtraction/Traits/IdentifierTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace HttpVerbExtraction\Test\Traits;
+
+class IdentifierTest extends \PHPUnit_Framework_TestCase {
+
+    private $resourceEvent;
+
+    public function setUp()
+    {
+        // TODO ResourceEvent mock
+    }
+
+    public function test_if_returns_identifier_as_null_when_not_specified()
+    {
+        $resourceEvent = $this->getMock('ZF\Rest\ResourceEvent');
+
+        $identifier = $this->getObjectForTrait('HttpVerbExtraction\Traits\Identifier');
+
+        $id = $identifier->getIdentifier($resourceEvent);
+        $this->assertNull($id);
+    }
+
+    public function test_if_returnes_specified_id_as_identifier()
+    {
+        $expectedId = 5;
+        $resourceEvent = $this->getMock('ZF\Rest\ResourceEvent');
+        $resourceEvent->method('getParam')
+            ->willReturn($expectedId);
+
+        $identifier = $this->getObjectForTrait('HttpVerbExtraction\Traits\Identifier');
+        
+        $id = $identifier->getIdentifier($resourceEvent);
+        $this->assertEquals($id, $expectedId);
+    }
+
+}

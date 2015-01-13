61c8fe34d27279205bae3224c52a1f6dc9b4818d
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-13 20:16:46 +0330

    Add phpunit bootstrap file

---
+++ tests/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+chdir(__DIR__);
+if (file_exists(__DIR__ . '/../vendor/autoload.php')){
+    require_once __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../../vendor/autoload.php';
+} else{
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
+$autoloader = new Zend\Loader\StandardAutoloader(array('autoregister_zf' => true));
+$autoloader->registerNamespace('HttpVerbExtraction', __DIR__ . '/../src/HttpVerbExtraction');
+$autoloader->register();

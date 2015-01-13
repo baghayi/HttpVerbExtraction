47333947079d7128bf1fcf1a6c036b5716a8bf9f
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-13 22:45:22 +0330

    If RouteMatch not found return immediately

--- src/HttpVerbExtraction/Service/ControllerName.php
+++ src/HttpVerbExtraction/Service/ControllerName.php
@@ -2,12 +2,16 @@
 namespace HttpVerbExtraction\Service;
 
 use ZF\Rest\ResourceEvent;
+use Zend\Mvc\Router\RouteMatch;
 
 final class ControllerName {
 
     public function get(ResourceEvent $event)
     {
         $routeMatch = $event->getRouteMatch();
+        if(!$routeMatch instanceof RouteMatch)
+            return null;
+
         return $routeMatch->getParam('controller', null);
     }
 

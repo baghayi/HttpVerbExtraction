396dd70d87fc9cc0bfcaf4c75c3fdb11705fffbe
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-13 21:06:45 +0330

    Testing InputFilter trait

---
+++ tests/HttpVerbExtraction/Traits/InputFilterTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace HttpVerbExtraction\Test\Traits;
+
+class InputFilterTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_if_returnes_inputfilter_object()
+    {
+        $inputFilterInterface = 'Zend\InputFilter\InputFilterInterface';
+        $inputFilterMock = $this->getMock($inputFilterInterface);
+
+        $resourceEvent = $this->getMock('ZF\Rest\ResourceEvent');
+        $resourceEvent->method('getInputFilter')
+            ->willReturn($inputFilterMock);
+
+        $inputFilter = $this->getObjectForTrait('HttpVerbExtraction\Traits\InputFilter');
+        
+        $returnedValue = $inputFilter->getInputFilter($resourceEvent);
+        $this->assertInstanceOf($inputFilterInterface, $returnedValue);
+    }
+
+}

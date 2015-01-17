fd075ea3073fff2aa5593def43ebdf7addffd147
Hossein Baghayi <hossein.baghayi@gmail.com>
2015-01-17 03:46:00 +0330

    Changed parameter name

--- src/HttpVerbExtraction/Exception/ServiceNameNotFound.php
+++ src/HttpVerbExtraction/Exception/ServiceNameNotFound.php
@@ -3,9 +3,9 @@ namespace HttpVerbExtraction\Exception;
 
 class ServiceNameNotFound extends \Exception {
 
-    public function __construct($verbServiceName)
+    public function __construct($verbName)
     {
-        $message = sprintf('Could not find service name for the requested "%s" verb.', $verbServiceName);
+        $message = sprintf('Could not find service name for the requested "%s" verb.', $verbName);
         parent::__construct($message);
     }
 
